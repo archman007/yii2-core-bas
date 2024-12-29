0987ae6757b03a42da5de8e299fbbc3330af49df
Arch Brooks <archman007@users.noreply.github.com>
2024-12-29 08:31:50 -0500

    Update db.php

--- modules/app1/config/db.php
+++ modules/app1/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
 'dsn' => 'mysql:host=localhost;dbname=bcswebtoolsx',
 'username' => 'bcs',
-'password' => 'Peace007!amb',
+'password' => 'your_password',
 'charset' => 'utf8',
 'attributes' => [
     PDO::ATTR_PERSISTENT => true,  // optional, for persistent connections
